8b2e52dda56a13fa1371e526caa497bcac8a3d51
droidark <droidark@outlook.com>
2017-05-12 09:32:59 -0500

    Practice 02 (Databinding) updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 import { PracticeTwoComponent } from './practice-two/practice-two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
     PracticeTwoComponent
   ],
   imports: [
