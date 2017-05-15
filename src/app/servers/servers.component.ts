e8d56214fd37904908da1121c8f581229fcf0a97
droidark <droidark@outlook.com>
2017-05-15 08:56:59 -0500

    Starting directives module

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created!';
-  serverName: string = 'Testserver';
+  serverName: string = '';
+  serverCreated: boolean = false;
 
   constructor() {
     /* 2.24  Property Binding
@@ -23,6 +24,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
